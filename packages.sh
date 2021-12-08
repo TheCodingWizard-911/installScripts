e1d2134020f57162e8612405492a7f4ddf58c8b3
Ganesh Gitte <ganeshs.gitte911@gmail.com>
2021-12-08 21:36:40 +0530

    initial commit

--- packages.sh
+++ packages.sh
@@ -10,11 +10,11 @@ sudo pacman -S firefox-developer-edition brave-browser alacritty neofetch stow v
 
 # installing packages from AUR
 
-sudo pamac install visual-studio-code-bin whitesur-gtk-theme-git whitesur-icon-theme-git --noconfirm
+sudo pamac build visual-studio-code-bin whitesur-gtk-theme-git whitesur-icon-theme-git --no-confirm
 
 # remove unwanted fonts
 sudo pacman -Rcns adobe-source-han-sans-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-han-sans-cn-fonts ttf-droid ttf-dejavu ttf-inconsolata ttf-indic-otf noto-fonts --noconfirm
 
 # install fonts
 sudo pacman -S ttf-meslo-nerd-font-powerlevel10k ttf-fira-code ttf-montserrat --noconfirm
-sudo pamac install ttf-bookerly
+sudo pamac build ttf-bookerly -no-confirm
